ae412fdb5c3b653db657c500cf050ccee6342572
summaiyasiddiqui <summaiyanadeem002@gmail.com>
2026-07-22 07:40:55 -0700

    Day 22: Dockerize FastAPI with automatic Alembic migrations

---
+++ backend/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Waiting for PostgreSQL..."
+
+sleep 5
+
+echo "Applying database migrations..."
+
+alembic upgrade head
+
+echo "Starting FastAPI..."
+
+exec uvicorn app.main:app --host 0.0.0.0 --port 8000
