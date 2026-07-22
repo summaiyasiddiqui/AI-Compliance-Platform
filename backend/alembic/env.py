d8b1ee78e79f13d3d15e3c32b8ff6271c5885745
summaiyasiddiqui <summaiyanadeem002@gmail.com>
2026-07-22 07:16:05 -0700

    Day 21 - Dockerize backend and configure Alembic migrations

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -5,10 +5,13 @@ from app.models.user import User
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from alembic import context
+from app.config import settings
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", settings.database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
