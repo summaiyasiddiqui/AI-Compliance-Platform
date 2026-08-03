ceb27f11934035ed8837d01ba610938335eaff36
summaiyasiddiqui <summaiyanadeem002@gmail.com>
2026-08-03 01:53:15 -0700

    fix: rebuild Alembic migration baseline

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -3,6 +3,7 @@ from logging.config import fileConfig
 from alembic import context
 from app.config import settings
 from app.database import Base
+from app.models import User, Company
 from sqlalchemy import engine_from_config, pool
 
 # this is the Alembic Config object, which provides
